953058c23c137290911b9a7a80300562f0b5650d
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-11 17:06:30 +0200

    added about me part

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,55 @@
 import React from "react";
+import ME from "../../assets/images/me-about.jpeg";
+import { FiAward } from "react-icons/fi";
+import { FiUsers } from "react-icons/fi";
+import { MdOutlineLibraryBooks } from "react-icons/md";
 import "./about.css";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get TO Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about-image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>+7 Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200 Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <MdOutlineLibraryBooks className="about__icon" />
+              <h5>Project</h5>
+              <small>+80</small>
+            </article>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
