c7d448a4108502903dbb6d3d30ac5d0e3ca148bb
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-11 15:06:14 +0200

    added useState to keeping the active classes

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { FiUser } from "react-icons/fi";
 import { RiMacbookLine } from "react-icons/ri";
@@ -7,21 +7,42 @@ import { AiOutlineContacts } from "react-icons/ai";
 import "./nav.css";
 
 const Nav = () => {
+  const [active, setActive] = useState("#");
   return (
     <nav>
-      <a href="#" className="link">
+      <a
+        href="#"
+        onClick={() => setActive("#")}
+        className={active === "#" ? "active link" : "link"}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about" className="link">
+      <a
+        href="#about"
+        onClick={() => setActive("#about")}
+        className={active === "#about" ? "active link" : "link"}
+      >
         <FiUser />
       </a>
-      <a href="#experience" className="link">
+      <a
+        href="#experience"
+        onClick={() => setActive("#experience")}
+        className={active === "#experience" ? "active link" : "link"}
+      >
         <RiMacbookLine />
       </a>
-      <a href="#services" className="link">
+      <a
+        href="#services"
+        onClick={() => setActive("#services")}
+        className={active === "#services" ? "active link" : "link"}
+      >
         <MdOutlineDesignServices />
       </a>
-      <a href="#contact" className="link">
+      <a
+        href="#contact"
+        onClick={() => setActive("#contact")}
+        className={active === "#contact" ? "active link" : "link"}
+      >
         <AiOutlineContacts />
       </a>
     </nav>
