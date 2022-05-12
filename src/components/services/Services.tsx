4b3628ac7f2709ab5dbb50cde92c1718d35aefdd
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-12 14:56:46 +0200

    added services section

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,7 +1,164 @@
 import React from "react";
+import { FaCheck } from "react-icons/fa";
 import "./services.css";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h4>What I Offer</h4>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
