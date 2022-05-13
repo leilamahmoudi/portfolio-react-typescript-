4ed8f0e48c20b88e4a43bccb33ab7cb02d0d3696
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-13 12:09:49 +0200

    added content of portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,8 +1,142 @@
 import React from "react";
+import IMG from "../../assets/images/img2.jpeg";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a
+            href="https://github.com/leilamahmoudi"
+            className="link btn"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="link btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
