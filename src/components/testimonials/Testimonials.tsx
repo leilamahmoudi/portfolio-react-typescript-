03ad1b06105b63a28f23b81855d1f48d39e2a256
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-16 10:07:01 +0200

    added testimonials dynamic content

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/images/me.jpeg";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "vegis",
+    review:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+  },
+  {
+    avatar: AVTR1,
+    name: "vegis",
+    review:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+  },
+  {
+    avatar: AVTR1,
+    name: "vegis",
+    review:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+  },
+  {
+    avatar: AVTR1,
+    name: "vegis",
+    review:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+  },
+];
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container testimonials__container">
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
