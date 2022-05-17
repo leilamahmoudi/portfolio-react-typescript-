11920426682cbc8c687d6115d2c38c12639c990d
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-17 11:09:39 +0200

    added some css to form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -13,31 +13,37 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <AiOutlineMail />
+            <AiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>leila.mahmoudi9169@gmail.com</h5>
-            <a href="mailto:leila.mahmoudi9169@gmail.com" target="_blank">
-              Send A Messeage
-            </a>
-          </article>
-          <article className="contact__option">
-            <FaFacebookMessenger />
-            <h4>Messenger</h4>
-            <h5>leila.mahmoudi9169@gmail.com</h5>
             <a
-              href="https://www.facebook.com/leila.mahmoudi.7739"
+              href="mailto:leila.mahmoudi9169@gmail.com"
               target="_blank"
+              className="link"
             >
               Send A Messeage
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <FaFacebookMessenger className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>leila.mahmoudi9169@gmail.com</h5>
+            <a
+              href="https://www.facebook.com/leila.mahmoudi.7739"
+              target="_blank"
+              className="link"
+            >
+              Send A Messeage
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
             <h4>AhatsApp</h4>
             <h5>+46729100426</h5>
             <a
               href="https://whatsapp.com/send?phone=+46729100426"
               target="_blank"
+              className="link"
             >
               Send A Messeage
             </a>
