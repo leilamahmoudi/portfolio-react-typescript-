596940cb98b66a0d62150048b5356933025ac89d
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-19 16:18:59 +0200

    updated portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -3,6 +3,7 @@ import IMG1 from "../../assets/images/vegis.png";
 import IMG2 from "../../assets/images/lending-app.png";
 import IMG3 from "../../assets/images/dictionary.png";
 import IMG4 from "../../assets/images/weather.png";
+import IMG5 from "../../assets/images/todo.png";
 import "./portfolio.css";
 
 const data = [
@@ -10,6 +11,7 @@ const data = [
     id: 1,
     img: IMG1,
     title: "Vegis",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/leilamahmoudi/vegis",
     demo: "https://vegist-leila.netlify.app/",
   },
@@ -17,6 +19,7 @@ const data = [
     id: 2,
     img: IMG2,
     title: "Lending-app",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/FeNatalia/lending-app",
     demo: "https://lending-app-codezilla.herokuapp.com/",
   },
@@ -24,6 +27,7 @@ const data = [
     id: 3,
     img: IMG3,
     title: "Dictionary",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/leilamahmoudi/dictionary-project",
     demo: "https://dictionary-leila.netlify.app/",
   },
@@ -31,13 +35,15 @@ const data = [
     id: 4,
     img: IMG4,
     title: "Weather",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/leilamahmoudi/test_project",
     demo: "https://weather-ver1-leila.netlify.app/",
   },
   {
     id: 5,
-    img: IMG1,
-    title: "vegis",
+    img: IMG5,
+    title: "todo",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/leilamahmoudi/vegis",
     demo: "https://vegist-leila.netlify.app/",
   },
@@ -45,6 +51,7 @@ const data = [
     id: 6,
     img: IMG1,
     title: "vegis",
+    tech: "MongoDB,Express.js,React ,Node.js",
     github: "https://github.com/leilamahmoudi/vegis",
     demo: "https://vegist-leila.netlify.app/",
   },
@@ -57,13 +64,14 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, img, title, github, demo }) => {
+        {data.map(({ id, img, title, github, demo, tech }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={img} alt={title} />
               </div>
               <h3>{title}</h3>
+              <h4>{tech}</h4>
               <div className="portfolio__item-cta">
                 <a href={github} className="btn" target="_blank">
                   Github
